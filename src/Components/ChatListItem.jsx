4a53e7dc647cec1da75a06994ded067748232ba4
Benjamin Akogwu <72498960+akbenngold@users.noreply.github.com>
2024-03-15 05:31:01 +0100

    Update ChatListItem.jsx

--- src/Components/ChatListItem.jsx
+++ src/Components/ChatListItem.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function ChatListItem() {
   return (
     <div className="user">
-      <img src="src/assets/avatar.jpeg" alt="" />
+      <img src="chat-app/src/assets/avatar.jpeg" alt="" />
       <div className="details">
         <span className="name">ojia</span>
         <span className="message-preview">heyyyy</span>
