a7c73f5259b918d75d7fb1be4c443640636cd9f7
Benjamin Akogwu <72498960+akbenngold@users.noreply.github.com>
2024-03-17 09:24:49 +0100

    Update message.jsx

--- src/Components/message.jsx
+++ src/Components/message.jsx
@@ -5,20 +5,20 @@ function Message() {
   return (
     <div>
       <div className="message owner">
-        <img className="message__img" src="src/assets/avatar.jpeg" alt="" />
+        <img className="message__img" src="https://unsplash.com/photos/shallow-focus-photography-of-woman-outdoor-during-day-rDEOVtE7vOs" alt="" />
 
         <div className="message__content">
-          <img src="src/assets/avatar.jpeg" alt="" />
+          <img src="https://unsplash.com/photos/shallow-focus-photography-of-woman-outdoor-during-day-rDEOVtE7vOs" alt="" />
           <div className="message__body">hello</div>
           <span className="timestamp">1:32</span>
         </div>
       </div>
 
       <div className="message">
-        <img className="message__img" src="src/assets/avatar.jpeg" alt="" />
+        <img className="message__img" src="https://unsplash.com/photos/shallow-focus-photography-of-woman-outdoor-during-day-rDEOVtE7vOs" alt="" />
 
         <div className="message__content">
-          <img src="src/assets/avatar.jpeg" alt="" />
+          <img src="https://unsplash.com/photos/shallow-focus-photography-of-woman-outdoor-during-day-rDEOVtE7vOs" alt="" />
           <div className="message__body">hello</div>
           <span className="timestamp">1:32</span>
         </div>
